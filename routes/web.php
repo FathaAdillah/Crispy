432e99f8c9d552e22cee2ca3b465ba894f4dd756
Fatha Adillah <fatha.adillah12@gmail.com>
2024-08-04 19:02:36 +0700

    Jawaban route

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
-use App\Http\Controllers\CodeController;
-use App\Http\Controllers\KuisionerController;
-use App\Http\Controllers\PertanyaanController;
-use App\Http\Controllers\ReportController;
-use App\Http\Controllers\RespondenController;
-use App\Http\Controllers\VariableController;
+use App\Http\Controllers\AnswerController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CodeController;
+use App\Http\Controllers\FileController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\VariableController;
+use App\Http\Controllers\KuisionerController;
+use App\Http\Controllers\RespondenController;
+use App\Http\Controllers\PertanyaanController;
 
 Route::get('/', [KuisionerController::class, 'index']);
 Route::get('/questions', [KuisionerController::class, 'index']);
 // Route::get('/question', [KuisionerController::class, 'index']);
+// Route::post('/file/upload', [FileController::class, 'upload'])->name('file.upload');
 Route::post('/submit-kuisioner', [KuisionerController::class, 'submit'])->name('submit-kuisioner');
 
 Route::get('/login', function () {
@@ -26,4 +29,6 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::resource('responden', RespondenController::class);
     Route::resource('report', ReportController::class);
     Route::resource('code', CodeController::class);
+    Route::resource('jawaban', AnswerController::class);
+    Route::get('/jawaban/{responden_id}', [AnswerController::class, 'show']);
 });
